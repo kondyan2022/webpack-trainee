5afbc0ac7e739d0f005f945cf294479a231c355a
kondyan2022 <kondyan.asusnexus@gmail.com>
2024-02-07 00:11:05 +0200

    Add sass cssmin

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import { useState } from "react";
+import "./App.scss";
 
 export const App = () => {
-  return <div>App</div>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <>
+      <div>App</div>
+      <div>{counter}</div>
+      <button
+        type="button"
+        onClick={() => {
+          setCounter((prev) => (prev += 1));
+        }}
+      >
+        inc
+      </button>
+    </>
+  );
 };
--- webpack.config.ts
+++ webpack.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 type Mode = "production" | "development";
 
@@ -12,6 +13,7 @@ interface EnvVariables {
 
 export default (env: EnvVariables) => {
   const isDev = env.mode === "development";
+  const isProd = !isDev;
   const config: webpack.Configuration = {
     mode: env.mode ?? "development",
     entry: path.resolve(__dirname, "src", "index.tsx"),
@@ -25,10 +27,23 @@ export default (env: EnvVariables) => {
         template: path.resolve(__dirname, "public", "index.html"),
       }),
       //slowly
-      new webpack.ProgressPlugin(),
+      isDev && new webpack.ProgressPlugin(),
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename: "css/[name].[contenthash:8].css",
+          chunkFilename: "css/[name].[contenthash:8].css",
+        }),
     ].filter(Boolean),
     module: {
       rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
         //ts-loader підтримує JSX. Якщо не Typescript то потрібен babel-loader
         {
           test: /\.tsx?$/,
