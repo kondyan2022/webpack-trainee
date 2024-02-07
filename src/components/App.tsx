606b4b3896b7c58fae6fe0da7c91026d2ebc65e5
kondyan2022 <kondyan.asusnexus@gmail.com>
2024-02-08 01:40:40 +0200

    Add react-router-dom

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import classes from "./App.module.scss";
+import { Link, Outlet } from "react-router-dom";
 
 export const App = () => {
   const [counter, setCounter] = useState(0);
@@ -7,6 +8,9 @@ export const App = () => {
   return (
     <>
       <div>App</div>
+      <Link to={"/about"}>About</Link>
+      <br />
+      <Link to={"/shop"}>Shop</Link>
       <div className={classes.value}>{counter}</div>
       <button
         className={classes.button}
@@ -17,6 +21,8 @@ export const App = () => {
       >
         inc
       </button>
+
+      <Outlet />
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import { Suspense } from "react";
+import { LazyAbout } from "./pages/about/About.lazy";
+import { LazyShop } from "./pages/shop/Shop.lazy";
 
 const root = document.getElementById("root");
 
@@ -7,5 +12,29 @@ if (!root) {
   throw new Error("Root not found");
 }
 const container = createRoot(root);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={"loading...."}>
+            <LazyAbout />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/shop",
+        element: (
+          <Suspense fallback={"loading...."}>
+            <LazyShop />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
 
-container.render(<App />);
+container.render(<RouterProvider router={router} />);
